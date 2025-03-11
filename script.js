f02adf682cbb97c49ef40a1f57e360f8c7e28ab8
Артур Ра <77981720+A-Ra7@users.noreply.github.com>
2025-03-11 17:17:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ cvs.addEventListener('touchstart', (event) => {
   moveUp();
   moveUpTouch = setInterval(function(){
     moveUp();
-  },200);
+  },120);
 })
 
 cvs.addEventListener('touchend', (event) => {
